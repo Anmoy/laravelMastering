da78652acc38ef7490cda9c8f3f7249fa61434d3
Kowshik_ChowdhuryRudra <103317916+Anmoy@users.noreply.github.com>
2024-05-14 22:19:28 +0600

    Add files via upload

---
+++ storage/framework/views/3e04fa34cc7075c2ea54cecb6123c497.php
@@ -0,0 +1,24 @@
+
+
+
+
+
+<?php $__env->startSection('footer'); ?>
+    <!-- ***** Footer Start ***** -->
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="col-lg-12">
+                    <p>Copyright &copy; 2020 Training Studio
+                    
+                    - Designed by <a rel="nofollow" href="https://templatemo.com" class="tm-text-link" target="_parent">TemplateMo</a></p>
+                    
+                    <!-- You shall support us a little via PayPal to info@templatemo.com -->
+                    
+                </div>
+            </div>
+        </div>
+    </footer>
+
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('frontend.master', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\xampp\htdocs\training_studio\resources\views/frontend/home.blade.php ENDPATH**/ ?>
